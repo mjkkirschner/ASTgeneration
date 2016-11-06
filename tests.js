18a4cd6b16599537300a73edb72197c6da6ccf89
Michael Kirschner <mjk.kirschner@gmail.com>
2016-11-05 20:33:46 -0400

    ignoreAdded

--- tests.js
+++ tests.js
@@ -1,15 +1,19 @@
 "use strict";
 var Lexer = require('./Lexer');
-function test() {
-    console.log("starting");
-    var lexer = new Lexer.Lexer("x=100; ");
-    console.log(lexer);
-    var t = lexer.get_next_token();
-    while (t.type != Lexer.EOF) {
-        console.log(t);
-        t = lexer.get_next_token();
-    }
-}
-;
-test();
+var chai = require('chai');
+var expect = chai.expect; // we are using the "expect" style of Chai
+describe('lexer', function () {
+    it('should parse a simple assigment correctly', function () {
+        var lexer = new Lexer.Lexer("x=100; ");
+        console.log(lexer);
+        var t = lexer.get_next_token();
+        var tokens = [];
+        while (t.type != Lexer.EOF) {
+            tokens.push(t);
+            console.log(t);
+            t = lexer.get_next_token();
+        }
+        expect(tokens.map(function (token) { return token.type; })).to.deep.equal([Lexer.ID, Lexer.ASSIGN, Lexer.INTEGER, Lexer.SEMI]);
+    });
+});
 //# sourceMappingURL=tests.js.map
--- tests.ts
+++ tests.ts
@@ -1,18 +1,20 @@
 import * as Parser from './Parser';
 import * as Lexer from './Lexer';
 import * as Interpreter from './interprerter';
+import * as chai from 'chai';
+var expect = chai.expect; // we are using the "expect" style of Chai
 
-function test() {
-    console.log("starting");
-    var lexer = new Lexer.Lexer("x=100; ");
-    console.log(lexer);
-    var t = lexer.get_next_token();
-while (t.type != Lexer.EOF){
-    console.log(t);
-    t = lexer.get_next_token();
-}
-
-
-};
-
-test();
+describe('lexer', function () {
+    it('should parse a simple assigment correctly', function () {
+        var lexer = new Lexer.Lexer("x=100; ");
+        console.log(lexer);
+        var t = lexer.get_next_token();
+        var tokens:Lexer.Token[] = [];
+        while (t.type != Lexer.EOF) {
+            tokens.push(t);
+            console.log(t);
+            t = lexer.get_next_token();
+        }
+        expect(tokens.map(token=>{return token.type})).to.deep.equal([Lexer.ID,Lexer.ASSIGN,Lexer.INTEGER,Lexer.SEMI]);
+    });
+});
